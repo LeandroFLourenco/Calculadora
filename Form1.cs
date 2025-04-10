a18238bab46b0f71a0b9b15848ca0a9504075896
leandrolourenco <leandro.francisco.lourenco@gmail.com>
2025-04-10 10:20:25 -0300

    feat: Resolvido problemas de branch e solution

--- Form1.cs
+++ Form1.cs
@@ -11,9 +11,30 @@ namespace Projeto_Cadastramento
 {
     public partial class Calculadora : Form
     {
+        double numAnterior = 0;
+        string operacao = "";
+        bool limparDisplay = false;
+
         public Calculadora()
         {
             InitializeComponent();
+        }
+        private void leituraBotoes(object sender, EventArgs e)
+        {
+            Button botao = sender as Button;
+            if (botao != null)
+            {
+                tbConta.Text += botao.Text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(tbConta.Text, out numAnterior))
+            {
+                operacao = "+";
+
+            }
         }
     }
 }
